824dd871d7487982c0170e76d19d21e442d0225a
wopow <wf.wopow@gmail.com>
2015-03-11 09:48:48 +0800

    itsm.py实现了在当前目录中所有的xls文件中统计各个协管员建单数量

---
+++ itsm.py
@@ -0,0 +1,43 @@
+#coding=utf8
+import xlrd
+import sys
+import os
+#解决编码问题
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+#建立空字典
+txtkey={  }
+#print输出改为蓝色
+def inblue( s ):
+    return"%s[1;36;2m%s%s[0m"%(chr(27), s, chr(27))
+
+filename=os.listdir(sys.argv[1])
+xlsfile=[]
+for i in filename:
+    j=i[i.rfind('.'):]
+    if j == '.xls':
+         xlsfile.append(i)
+sum=0
+for x in xlsfile:
+	#读取命令行中的路径，并打开文件
+	data=xlrd.open_workbook(sys.argv[1]+ "/" + x)
+	#选择工作簿
+	table=data.sheets()[0]
+	#选择数据列
+	txt=table.col_values(2)
+
+	#遍历数据，并加入字典，统计key出现的次数
+	for i in txt:
+	    if txtkey.has_key(i):
+		txtkey[i]=txtkey.get(i)+1
+	    else:
+		txtkey.setdefault(i,1)
+#打开日志文件，并写入
+file=open('itsm.log',"w")
+for j in txtkey:
+    sum=txtkey[j]+sum
+    print u"<%s>服务请求建单数为：%s" %(inblue(j),inblue(txtkey[j]))
+    itsm=u"<%s>服务请求建单数为：%s\r\n" %(j,txtkey[j])
+    file.write(itsm)
+file.write("共合计："+str(sum)+"笔")
+file.close()
